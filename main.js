330f4464a2baff562c50d4bd75e0d4e6954f1421
Ma Tko <kwikzzstep@gmail.com>
2021-09-20 08:15:16 +0200

    20th commit

---
+++ main.js
@@ -0,0 +1,25 @@
+
+var products = {
+
+    keyboard1: {
+      name : "",
+      desc : "Greatest properly off ham exercise all.",
+      img : "dummy-pdt-b.jpg",
+      price : 2034
+    },
+
+    keyboard2: {
+      name : "MokBook Rookie",
+      desc : "Unsatiable its possession nor off.",
+      img : "dummy-pdt-b.jpg",
+      price : 1247
+    },
+
+    keyboard3: {
+      name : "iPong Max",
+      desc : "All difficulty unreserved the solicitude.",
+      img : "dummy-pdt-a.jpg",
+      price : 675
+    }
+    
+  };

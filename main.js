b2c9e89316018e450cbcf7a8370728db0977324a
Ma Tko <kwikzzstep@gmail.com>
2021-09-21 08:13:03 +0200

    23rd commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const email = document.getElementById('email');
 
 function subscribe(){
 
-    if(email == ""){
+    if(email.value == ""){
         alert('Please write your email here!')
     } else {
         return true;
@@ -12,27 +12,3 @@ function subscribe(){
 }
 
 
-var products = {
-
-    keyboard1: {
-      name : "Corsair K65 RGB Mini Gaming Keyboard",
-      desc : "Greatest properly off ham exercise all.",
-      img : "dummy-pdt-b.jpg",
-      price : 2034
-    },
-
-    keyboard2: {
-      name : "HyperX Alloy Origins 60 Gaming Keyboard",
-      desc : "Unsatiable its possession nor off.",
-      img : "dummy-pdt-b.jpg",
-      price : 1247
-    },
-
-    keyboard3: {
-      name : "Fnatic Streak65 Gaming Keyboard",
-      desc : "All difficulty unreserved the solicitude.",
-      img : "dummy-pdt-a.jpg",
-      price : 675
-    }
-    
-  };
